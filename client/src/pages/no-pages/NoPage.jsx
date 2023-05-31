38dc25ae8bcae798ccdeca240e40ae1fac884291
Joel Siroy <kodesimple57@gmail.com>
2023-05-31 12:27:26 +0800

    joel- some update on layout

--- client/src/pages/no-pages/NoPage.jsx
+++ client/src/pages/no-pages/NoPage.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 function NoPage() {
   return (
-    <div>
-        <h2>Error 404: Page not found</h2>
-    </div>
+    <>
+
+      <div class="d-flex justify-content-center align-self-center pt-5 container-fluid">
+           <div class="pt-5 w-50 bg-body text-center">
+             <h1>Error 404: Page not found</h1>
+                {/* no page content starts here */}
+
+            </div>
+       </div>
+      <div>
+        
+       </div>
+    </>   
   )
+
+    
 }
 
 export default NoPage
