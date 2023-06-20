9b6f5e99f9b00d0f57144162a9915d87b8ed2fab
Joel Siroy <kodesimple57@gmail.com>
2023-06-20 18:40:17 +0800

    change public ip

--- src/api-service/apiService.js
+++ src/api-service/apiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://3.92.136.168:8000',
+    // baseURL: 'http://localhost:8000',
 });
 
 export default apiService;
