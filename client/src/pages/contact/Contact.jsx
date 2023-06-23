38ed66097b731e09fbc8e7ed3b63deed1af9a920
unknown <kodesimple57@gmail.com>
2023-06-23 16:29:09 +0800

    jinky first commit

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -3,13 +3,67 @@ import NavBarComp from '../../components/header/NavBarComp';
 import Footer from '../../components/footer/Footer';
 import AiFillFacebook from '../../components/resources/fb-icon.png';
 import AiFillInstagram from '../../components/resources/insta-icon.png';
-import { Form, Button} from 'react-bootstrap';
+import { Form, Button, Spinner, Modal} from 'react-bootstrap';
 import { Badge } from 'react-bootstrap';
 import '../contact/contact.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+
 // import { AiFillFacebook, AiFillInstagram } from 'react-icons/ai';
 
 function Contact() {
+
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    // const emailData = {
+    //   To_email: 'kodesimple57@gmail.com',
+    //   email_contact: event.target.email.value,
+    //   client_phone: event.target.contactNumber.value,
+    //   email_subject: event.target.subject.value,
+    //   from_name: event.target.fullName.value,
+    //   concern_message: event.target.concernMessage.value
+    // };
+
+    const emailData = {
+      To_email: 'jonescarla2022@gmail.com',
+      email_contact: event.target.emailPublic.value,
+      client_phone: event.target.phonePublic.value,
+      //email_subject: event.target.subject.value,
+      email_subject:'Inquriy',
+      from_name: event.target.nameFirst.value,
+      company_name: event.target.companyPublic.value,
+      industry_name: event.target.industryPublic.value,
+      annual_revenue: event.target.revenuePublic.value,
+      name_country: event.target.countryPublic.value,
+      concern_message: event.target.formMessage.value
+    };
+
+    emailjs
+      .send('service_dshiyj8', 'template_66cq8zg', emailData, 'y8KcAyItpP1kCao23')
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        setLoading(false);
+        setShowModal(true);
+        event.target.reset();
+        setTimeout(() => {
+          setShowModal(false);
+        }, 5000);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        setLoading(false);
+      });
+  };
+
+
+
   
   return(
      <> 
@@ -73,7 +127,7 @@ function Contact() {
           <iframe
             title="Map"
             width="100%"
-            height="300"
+            height="200"
             frameBorder="0"
             scrolling="no"
             marginHeight="0"
@@ -97,88 +151,118 @@ function Contact() {
         </Col>
 
         {/* Second Column */}
-        <Col sm={6} style={{ padding: '20px' }}>
+        <Col sm={6} lg={6} style={{ padding: '20px' }}>
          
-          <Form className="contact-item">
+          <Form className="contact-item" onSubmit={handleSubmit}>
             <Row>
-              <Col>
-                <Form.Group controlId="firstName">
+              <Col sm={6}>
+                <Form.Group controlId="nameFirst">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter first name" style={{ marginBottom: '10px' }}/>
+                  <Form.Control name="nameFirst" type="text" placeholder="Enter first name" style={{ marginBottom: '10px' }}/>
                 </Form.Group>
               </Col>
-              <Col>
+              <Col sm={6}>
                 <Form.Group controlId="lastName">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter last name" style={{ marginBottom: '10px' }}/>
+                  <Form.Control name="lastName" type="text" placeholder="Enter last name" style={{ marginBottom: '10px' }}/>
                 </Form.Group>
               </Col>
             </Row>
 
             <Row>
-            <Col>
-            <Form.Group controlId="email">
+            <Col sm={6}>
+            <Form.Group controlId="emailPublic">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" style={{ marginBottom: '10px' }}/>
+              <Form.Control name="emailPublic" type="email" placeholder="Enter email" style={{ marginBottom: '10px' }}/>
             </Form.Group>
             </Col>
 
-            <Col>
-            <Form.Group controlId="phone">
+            <Col sm={6}>
+            <Form.Group controlId="phonePublic">
               <Form.Label>Phone</Form.Label>
-              <Form.Control type="text" placeholder="Enter phone number" style={{ marginBottom: '10px' }}/>
+              <Form.Control name="phonePublic" type="text" placeholder="Enter phone number" style={{ marginBottom: '10px' }}/>
             </Form.Group>
             </Col>
             </Row>
 
             <Row>
-              <Col>
-                <Form.Group controlId="company">
+              <Col sm={6}>
+                <Form.Group controlId="companyPublic">
                   <Form.Label>Company Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter company name" style={{ marginBottom: '10px' }}/>
+                  <Form.Control name="companyPublic" type="text" placeholder="Enter company name" style={{ marginBottom: '10px' }}/>
                 </Form.Group>
              </Col>
 
-              <Col>
-                <Form.Group controlId="industry">
+              <Col sm={6}>
+                <Form.Group controlId="industryPublic">
                   <Form.Label>Industry</Form.Label>
-                  <Form.Control type="text" placeholder="Enter industry" style={{ marginBottom: '10px' }}/>
+                  <Form.Control name="industryPublic" ype="text" placeholder="Enter industry" style={{ marginBottom: '10px' }}/>
                 </Form.Group>
               </Col>
             </Row>
 
             <Row>
-              <Col>
-                <Form.Group controlId="revenue">
+              <Col sm={6}>
+                <Form.Group controlId="revenuePublic">
                   <Form.Label>Annual Revenue</Form.Label>
-                  <Form.Control type="text" placeholder="Select Revenue" style={{ marginBottom: '10px' }}/>
+                  <Form.Control name="revenuePublic" type="text" placeholder="Select Revenue" style={{ marginBottom: '10px' }}/>
                 </Form.Group>
               </Col>
 
-              <Col>
-                <Form.Group controlId="country">
+              <Col sm={6}>
+                <Form.Group controlId="countryPublic">
                   <Form.Label>Country</Form.Label>
-                  <Form.Control type="text" placeholder="Enter country" style={{ marginBottom: '25px' }}/>
+                  <Form.Control name="countryPublic" type="text" placeholder="Enter country" style={{ marginBottom: '25px' }}/>
                 </Form.Group>
               </Col>
             </Row>
 
-            <Form.Group controlId="newsletter">
+            {/* <Form.Group controlId="newsletter">
               <Form.Check type="checkbox" label="Yes, I would like to receive news and offers from KodeSimple via email" style={{ marginBottom: '10px' }}/>
             </Form.Group>
 
             <Form.Group controlId="phoneCall">
               <Form.Check type="checkbox" label="Yes, I agree to receive phone calls from KodeSimple" style={{ marginBottom: '15px' }}/>
-            </Form.Group>
+            </Form.Group> */}
+
+            {/* <Form.Group controlId="subject">
+              <Form.Label>Subject:</Form.Label>
+              <Form.Control type="text" required placeholder="Enter your query" style={{ marginBottom: '10px' }}/>
+            </Form.Group> */}
 
             <Form.Group controlId="formMessage">
                   <Form.Label>Message</Form.Label>
                   <Form.Control as="textarea" rows={3} placeholder="Enter your message" style={{ marginBottom: '10px' }}/>
             </Form.Group>
 
-            <Button className="button-form" variant="primary" type="submit">
-                  Submit
-            </Button>
+            
+        
+
+            {/* <Button className="button-form" variant="primary" type="submit">
+            <a href="mailto:kodesimple57@gmail.com"/>Submit
+            </Button> */}
+
+<Button className="submit-concern-buton" type="submit" disabled={loading}>
+        {loading ? (
+          <>
+            <Spinner className="contact-button-spinner" animation="border" size="sm" /> Submitting...
+          </>
+        ) : (
+          'Submit'
+        )}
+      </Button>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Email Sent</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Your email has been sent successfully. Thank you!
+        </Modal.Body>
+      </Modal>
+
+
+            
           </Form>
         </Col>
       </Row>
